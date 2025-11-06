1a9cc260eefd31f07933e7db819bdef234947525
jamalovv3 <teymurrj@code.edu.az>
2025-11-06 17:21:23 +0400

    added

---
+++ New folder/RestaurantApp.DLL/RestaurantApp.Core/Models/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Core.Models
+
+    public class OrderItem
+{
+    public int Id { get; set; }
+    public MenuItem MenuItem { get; set; }
+    public int Count { get; set; }
+
+    public int MenuItemId { get; set; }
+
+    public int OrderId { get; set; }
+
+    public Order Order { get; set; }
+
+    {
+    }
+}
---
+++ New folder/RestaurantApp.DLL/RestaurantApp.DLL/Data/RestaurantDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantApp.DLL.Data
+{
+    public class RestaurantDbContext:DbContext
+    {
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+        public DbSet<MenuItem> MenuItems { get; set; }
+
+
+    }
+}
